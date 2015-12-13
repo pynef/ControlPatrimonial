5cdde62914178e21856199b6a15c0f50d3ef7450
Arturo Black <arturo.black0@gmail.com>
2015-12-13 11:43:09 -0500

    fix / in main url

--- ControlPatrimonial/urls.py
+++ ControlPatrimonial/urls.py
@@ -4,5 +4,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^rest/', include('app.ApiRest.urls')),
-    url(r'^/', include('FrontEnd.urls')),
+    url(r'^', include('FrontEnd.urls')),
 ]
