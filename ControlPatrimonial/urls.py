d8fb3457f1ebd699a4ea539213b77a67dc7ca9d4
nefi <nefinef@gmail.com>
2015-12-16 23:37:08 -0500

    restructurando el proyecto, creacion de apis de institucion, proveedores,

--- ControlPatrimonial/urls.py
+++ ControlPatrimonial/urls.py
@@ -4,10 +4,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    url(r'^CatalogoBienes/', include('app.ApiRest.CatalogoBienes.urls')),
-    url(r'^Empresa/', include('app.ApiRest.Institucion.urls')),
-    url(r'^Contabilidad/', include('app.ApiRest.Contabilidad.urls')),
-
-    url(r'^rest/', include('app.ApiRest.urls')),
-    url(r'^', include('FrontEnd.urls')),
+    url(r'^rest/', include('ApiRest.urls')),
+    url(r'/^', include('FrontEnd.urls')),
 ]
--- app/ApiRest/RecursosHumanos/serializers.py
+++
@@ -1,4 +0,0 @@
-from django.forms import widgets
-from rest_framework import serializers
-from django.contrib.auth.models import User
-from rest_framework.serializers import ModelSerializer
